c48660d2a3c5aab95fbef88ec089808d20d61aae
Michael JasonSmith <mpj17@onlinegroups.net>
2014-07-17 15:12:32 -0700

    PEP-8 OCD

--- setup.py
+++ setup.py
@@ -1,41 +1,62 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
-setup(name='gs.group.type.support',
-    version=version,
-    description='Support for "support" groups in GroupServer',
-    long_description=open("README.txt").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read(),
-    classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: Other/Proprietary License",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords='user, group, page',
-    author='Michael JasonSmith',
-    author_email='mpj17@onlinegroups.net',
-    url='http://groupserver.org',
-    license='ZPL',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.type',],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
+name = 'gs.group.type.support'
+url = 'https://source.iopen.net/groupserver/{0}/'.format(name)
+
+setup(name=name,
+      version=version,
+      description='Support for "support" groups in GroupServer',
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "Intended Audience :: Developers",
+          'License :: OSI Approved :: Zope Public License',
+          "Natural Language :: English",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='group, announcement, posts, topics, canpost',
+      author='Michael JasonSmith',
+      author_email='mpj17@onlinegroups.net',
+      url=url,
+      license='ZPL 2.1',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gs', 'gs.group', 'gs.group.type', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
         'setuptools',
         'zope.app.content',
         'gs.group.base',
-    ],
-    entry_points="""
-    # -*- Entry points: -*-
-    """,
-)
+      ],
+      entry_points="""
+          # -*- Entry points: -*-
+      """,)
