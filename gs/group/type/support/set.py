3d6325bd887e6d5b4e6cc8ccaa72845fb290c26d
Michael JasonSmith <mpj17@onlinegroups.net>
2014-07-18 12:20:30 -0700

    Adding the UnsetSupportGroup.

--- gs/group/type/support/set.py
+++ gs/group/type/support/set.py
@@ -13,13 +13,12 @@
 #
 ############################################################################
 from __future__ import absolute_import, unicode_literals
-from gs.group.type.set import SetABC
+from gs.group.type.set import (SetABC, UnsetABC)
 
 
 class SetSupportGroup(SetABC):
     'Set a group folder to be a support group'
     name = 'Support group'
-    typeId = 'gs-group-type-support'
     weight = 30
     show = True
 
@@ -27,4 +26,13 @@ class SetSupportGroup(SetABC):
         '''Add the marker-interface to make the group into a support
         group.'''
         iFaces = ['gs.group.type.support.interfaces.IGSSupportGroup']
-        self.add_marker_interfaces(self.group, iFaces)
+        self.add_marker(self.group, iFaces)
+
+
+class UnsetSupportGroup(UnsetABC):
+    name = 'Suppport group'
+    setTypeId = 'gs-group-type-support-set'
+
+    def unset(self):
+        iFaces = ['gs.group.type.support.interfaces.IGSSupportGroup']
+        self.del_marker(self.group, iFaces)
