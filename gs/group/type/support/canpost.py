521fc3ab85f4afc62b7f40a22e0686854a2668c1
Michael JasonSmith <mpj17@onlinegroups.net>
2014-07-21 13:46:09 -0700

    Getting ugly with the PEP-8 stick.

--- gs/group/type/support/canpost.py
+++ gs/group/type/support/canpost.py
@@ -1,11 +1,10 @@
 # coding=utf-8
-from operator import and_
+from __future__ import absolute_import, unicode_literals
+from operator import and_, attrgetter
 from zope.cachedescriptors.property import Lazy
-from zope.component import adapts, getGlobalSiteManager, getAdapters
-from zope.interface import implements, providedBy
-from Products.CustomUserFolder.interfaces import IGSUserInfo
-from gs.group.member.canpost.interfaces import IGSPostingUser, IGSCanPostRule
-from interfaces import IGSSupportGroup
+from zope.component import getGlobalSiteManager
+from gs.group.member.canpost.interfaces import IGSCanPostRule
+
 
 class CanPostToSupport(object):
     def __init__(self, userInfo, supportGroup):
@@ -15,27 +14,26 @@ class CanPostToSupport(object):
     @Lazy
     def rules(self):
         gsm = getGlobalSiteManager()
-        retval =  [instance for name, instance in 
-                    gsm.getAdapters((self.userInfo, self.group), 
-                                    IGSCanPostRule)]
-        retval.sort(key=lambda r:r.weight)
+        retval = [instance for name, instance in
+                  gsm.getAdapters((self.userInfo, self.group),
+                                  IGSCanPostRule)]
+        retval.sort(key=attrgetter('weight'))
         return retval
-    
+
     @Lazy
     def canPost(self):
         return reduce(and_, [rule.canPost for rule in self.rules], True)
-    
+
     @Lazy
     def statusNum(self):
-        statusNums = [rule.statusNum for rule in self.rules 
-                        if rule.statusNum != 0]
+        statusNums = [rule.statusNum for rule in self.rules
+                      if rule.statusNum != 0]
         retval = (statusNums and min(statusNums)) or 0
         assert retval >= 0
         return retval
-    
+
     @Lazy
     def status(self):
-        retval = [rule.status for rule in self.rules 
-                    if rule.statusNum == self.statusNum][0]
+        retval = [rule.status for rule in self.rules
+                  if rule.statusNum == self.statusNum][0]
         return retval
-
